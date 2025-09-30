3866ef346cdf1d92854680a92bba57d22db440a7
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2025-09-30 09:41:34 +0530

    Remove all files from repo

--- router.js
+++
@@ -1,9 +0,0 @@
-// router.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('server is up and running');
-});
-
-module.exports = router;
